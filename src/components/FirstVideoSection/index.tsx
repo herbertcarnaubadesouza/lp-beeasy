76ad553fa1e2d59152b9b0a8d47898d8223118d6
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2023-11-25 01:04:12 -0300

    fita rodando

---
+++ src/components/FirstVideoSection/index.tsx
@@ -0,0 +1,37 @@
+import styles from "./styles.module.scss";
+
+export default function FirtVideo() {
+  return (
+    <>
+      <section className={styles.container}>
+        <img src="/foto1.webp" />
+      </section>
+      <div className={styles.crossSection}>
+        <div className={styles.pinkRow}>
+          <div className={styles.marquee}>
+            <img src="/diamondWhite.svg" />
+            <span className={styles.marqueeStyle}>
+              SUA MARCA PERSONALIZADA COM NOSSA QUALIDADE
+            </span>
+            <img src="/diamondWhite.svg" />
+            <span className={styles.marqueeStyle}>
+              SUA MARCA PERSONALIZADA COM NOSSA QUALIDADE
+            </span>
+          </div>
+        </div>
+        <div className={styles.whiteRow}>
+          <div className={styles.marquee}>
+            <img src="/diamondBlack.svg" />
+            <span className={styles.marqueeStyle}>
+              SUA MARCA PERSONALIZADA COM NOSSA QUALIDADE
+            </span>
+            <img src="/diamondBlack.svg" />
+            <span className={styles.marqueeStyle}>
+              SUA MARCA PERSONALIZADA COM NOSSA QUALIDADE
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import Banner from "@/components/Banner";
+import FirtVideo from "@/components/FirstVideoSection";
 
 export default function Home() {
   return (
     <>
       <Banner />
+      <FirtVideo />
     </>
   );
 }
